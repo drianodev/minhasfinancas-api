f2c51ece23d20af2b1d2ed98fafbd743b6b6377d
drianodev <adrianomendes661@gmail.com>
2023-01-31 16:36:29 -0300

    lançamentos e testes

--- src/main/java/com/driano/minhasFinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/driano/minhasFinancas/api/resource/UsuarioResource.java
@@ -1,26 +1,28 @@
 package com.driano.minhasFinancas.api.resource;
 
+import com.driano.minhasFinancas.api.dto.TokenDTO;
 import com.driano.minhasFinancas.api.dto.UsuarioDTO;
 import com.driano.minhasFinancas.exception.ErroAutenticacao;
 import com.driano.minhasFinancas.exception.RegraNegocioException;
 import com.driano.minhasFinancas.model.entity.Usuario;
+import com.driano.minhasFinancas.service.LancamentoService;
 import com.driano.minhasFinancas.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
 public class UsuarioResource {
 
-    private UsuarioService service;
-
-    public UsuarioResource(UsuarioService service) {
-        this.service = service;
-    }
+    private final UsuarioService service;
+    private final LancamentoService lancamentoService;
+    //private final JwtService jwtService;
 
     @PostMapping("/autenticar")
     public ResponseEntity<?> autenticar( @RequestBody UsuarioDTO dto ) {
@@ -35,19 +37,32 @@ public class UsuarioResource {
     }
 
     @PostMapping
-    public ResponseEntity salvar( @RequestBody UsuarioDTO dto) {
+    public ResponseEntity salvar( @RequestBody UsuarioDTO dto ) {
 
         Usuario usuario = Usuario.builder()
-                    .nome(dto.getNome())
-                    .email(dto.getEmail())
-                    .senha(dto.getSenha())
-                    .build();
+                .nome(dto.getNome())
+                .email(dto.getEmail())
+                .senha(dto.getSenha()).build();
 
         try {
             Usuario usuarioSalvo = service.salvarUsuario(usuario);
             return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
-        } catch (RegraNegocioException e) {
+        }catch (RegraNegocioException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+
     }
+
+    @GetMapping("{id}/saldo")
+    public ResponseEntity obterSaldo( @PathVariable("id") Long id ) {
+        Optional<Usuario> usuario = service.obterPorId(id);
+
+        if(!usuario.isPresent()) {
+            return new ResponseEntity( HttpStatus.NOT_FOUND );
+        }
+
+        BigDecimal saldo = lancamentoService.obterSaldoPorUsuario(id);
+        return ResponseEntity.ok(saldo);
+    }
+
 }
---
+++ src/main/java/com/driano/minhasFinancas/service/JwtService.java
@@ -0,0 +1,18 @@
+package com.driano.minhasFinancas.service;
+
+import com.driano.minhasFinancas.model.entity.Usuario;
+/*
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+
+public interface JwtService {
+
+    String gerarToken(Usuario usuario);
+
+    Claims obterClaims(String token) throws ExpiredJwtException;
+
+    boolean isTokenValido(String token);
+
+    String obterLoginUsuario( String token );
+}
+*/
